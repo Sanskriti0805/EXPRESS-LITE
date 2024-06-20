ccb9c2f1f9fe40f234c44a987341fe3d340d352e
Sanskriti Arora <Sanskritiarora2005@gmail.com>
2024-06-20 22:55:21 +0530

    application

--- src/Application.ts
+++ src/Application.ts
@@ -1,81 +1,90 @@
 import { Server } from "http";
 import type Request from "./request";
 import type Response from "./response";
-import  {Router}  from "./router";
+import { Router } from "./router";
 import http from "http";
 import type net from "net";
 
 class Application extends Server {
-    private static instance: Application;
-    private router: Router ; 
-    private errorHandler: (error: Error, req: Request, res: Response) => void;
-  
-    static getInstance(input: {
-      errorHandler?: (error: Error, req: Request, res: Response) => void;
-      args?: unknown[];
-    }) {
-      if (!Application.instance) {
-        Application.instance = new Application();
-        Application.instance.listen(input.args);
-        if (input.errorHandler) {
-          Application.instance.errorHandler = input.errorHandler;
-        }
-      }
-      return Application.instance;
-    }
-      
-    listen(...args: any[]): Promise<void> {
-        const server = http.createServer(this);
-        server.addListener("request", this.handler.bind(this));
-      
-        return new Promise((resolve, reject) => {
-          server.listen(...args, (error?: Error) => {
-            if (error) {
-              reject(error);
-            } else {
-              resolve(); // Server listening successfully
-            }
-          });
-        });
-    }
-  
-    handler(req: Request, res: Response) {
-      const errorHandler = this.errorHandler || this.defaultErrorHandler;
-      const router = this.router ?? new Router(); 
-      router.handle(req, res, errorHandler);
-    }
-  
-    defaultErrorHandler(error: Error, _req: Request, res: Response) {
-      console.error("Error in route dispatch:", error);
-      res.status(500).send({ message: "Internal Server Error" });
-    }
-  
-    private lazyRouter() {
-      if (!this.router) {
-        this.router = new Router();
+  private static instance: Application;
+  private router: Router;
+  private errorHandler: (error: Error, req: Request, res: Response) => void;
+
+  static getInstance(input: {
+    errorHandler?: (error: Error, req: Request, res: Response) => void;
+    args?: unknown[];
+  }) {
+    if (!Application.instance) {
+      Application.instance = new Application();
+      Application.instance.listen(input.args);
+      if (input.errorHandler) {
+        Application.instance.errorHandler = input.errorHandler;
       }
     }
-  
-    public get(path: string, callback: (req: Request, res: Response) => void) {
-      this.lazyRouter(); // Ensuring router is initialized before adding route
-      this.router.get(path, callback);
-    }
-  
-    public put(path: string, callback: (req: Request, res: Response) => void) {
-      this.lazyRouter();
-      this.router.put(path, callback);
-    }
-  
-    public delete(path: string, callback: (req: Request, res: Response) => void) {
-      this.lazyRouter();
-      this.router.delete(path, callback);
-    }
-  
-    public post(path: string, callback: (req: Request, res: Response) => void) {
-      this.lazyRouter();
-      this.router.post(path, callback);
+    return Application.instance;
+  }
+  listen(
+    port?: number,
+    hostname?: string,
+    backlog?: number,
+    listeningListener?: () => void,
+  ): this;
+  listen(
+    port?: number,
+    hostname?: string,
+    listeningListener?: () => void,
+  ): this;
+  listen(port?: number, backlog?: number, listeningListener?: () => void): this;
+  listen(port?: number, listeningListener?: () => void): this;
+  listen(path: string, backlog?: number, listeningListener?: () => void): this;
+  listen(path: string, listeningListener?: () => void): this;
+  listen(options: net.ListenOptions, listeningListener?: () => void): this;
+  listen(handle: any, backlog?: number, listeningListener?: () => void): this;
+  listen(handle: any, listeningListener?: () => void): this;
+  listen(...args: any[]) {
+    const server = http.createServer(this);
+    server.addListener("request", this.handler.bind(this));
+
+
+    return server.listen.apply(server, args);
+  }
+
+  handler(req: Request, res: Response) {
+    const errorHandler = this.errorHandler || this.defaultErrorHandler;
+    const router = this.router ?? new Router();
+    router.handle(req, res, errorHandler);
+  }
+
+  defaultErrorHandler(error: Error, _req: Request, res: Response) {
+    console.error("Error in route dispatch:", error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+
+  private lazyRouter() {
+    if (!this.router) {
+      this.router = new Router();
     }
   }
-  
-  export default Application;
-  
+
+  public get(path: string, callback: (req: Request, res: Response) => void) {
+    this.lazyRouter(); // Ensuring router is initialized before adding route
+    this.router.get(path, callback);
+  }
+
+  public put(path: string, callback: (req: Request, res: Response) => void) {
+    this.lazyRouter();
+    this.router.put(path, callback);
+  }
+
+  public delete(path: string, callback: (req: Request, res: Response) => void) {
+    this.lazyRouter();
+    this.router.delete(path, callback);
+  }
+
+  public post(path: string, callback: (req: Request, res: Response) => void) {
+    this.lazyRouter();
+    this.router.post(path, callback);
+  }
+}
+
+export default Application;
