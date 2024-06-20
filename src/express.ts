50d0f4b41348d6df281d56c77d85f0d8c852bca7
Sanskriti Arora <Sanskritiarora2005@gmail.com>
2024-06-20 22:26:59 +0530

    express

--- src/express.ts
+++ src/express.ts
@@ -1,17 +1,46 @@
-import Application  from "./Application";
-import Request from "./request";
+import Application from "./Application";
+import Request from "./request"; 
 import Response from "./response";
 
-function errorHandler(error: Error, req: Request, res: Response) {
-
-
+function errorHandler(error: Error, _req: Request, res: Response) {
+  console.error("Error:", error); 
+  res.status(500).send({ message: "Internal Server Error" }); 
 }
 
-function start() {
-    Application.getInstance({
-        errorHandler,
-        args: [3000],
-    })
-}
+class ExpressServer { 
+  private app: Application;
 
-start();
+  constructor() {
+    this.app = Application.getInstance({
+      errorHandler, 
+      args: [3000], 
+    });
+  }
+
+  public get(path: string, callback: (req: Request, res: Response) => void) {
+    this.app.get(path, callback); 
+  }
+
+  public post(path: string, callback: (req: Request, res: Response) => void) {
+    this.app.post(path, callback);
+  }
+
+  public put(path: string, callback: (req: Request, res: Response) => void) {
+    this.app.put(path, callback); 
+  }
+
+    public start() {
+    this.app.listen(); 
+}
+}
+const server = new ExpressServer();
+// Example PUT route 
+server.put("/data", (req, res) => {
+  
+ const data = req.body;
+ console.log("Received data in PUT request:", data);
+
+ res.send("Data received successfully!");
+});
+
+server.start();
